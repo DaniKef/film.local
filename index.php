9264f41adcac8e0d8504712a0cac80316bbc5ac8
Daniil Hurenko <gurenko888@gmail.com>
2025-12-28 19:06:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Daniil Hurenko">
+    <meta name="description" content="Site for films, serials.">
+    <title>SFilms</title>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style/css/main.css" type="text/css">
+</head>
+<body>
+	<header id="header">
+        <div id="logo-container">
+               <a href="index.php"><img id="site-image" src="images/1.svg" 
+                alt="theater_masks"></a> 
+        </div>
+        <nav id="sections-links">
+            <ul>
+                <li><a href="">Фильмы</a></li>
+                <li><a href="">Сериалы</a></li>
+            </ul>
+        </nav>
+    </header>
+	<main id="main">
+        <p>Lorem ipsum dolor sit ame t consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cum inventore nostrum asperiores. Deserunt modi dignissimos ad delectus illo, 
+            numquam laboriosam incidunt atque ducimus cupiditate ea totam expedita, aspernatur, 
+            vero fuga?</p>
+    </main>
+	<footer id="footer">
+        <p>SFilms</p>
+    </footer>
+</body>
+</html>
